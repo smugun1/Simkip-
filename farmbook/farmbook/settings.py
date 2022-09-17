b2a9523eac6845057e3c132d2926d2ece94732bd
smugun2013@hotmail.com <smugun2013@gmail.com>
2022-09-17 16:47:24 +0300

    Added git ignore file, requrements file and Postgressql db"

--- farmbook/farmbook/settings.py
+++ farmbook/farmbook/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'farmbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'First_Test',
+        'USER': 'postgres',
+        'PASSWORD': 'P*sng+2021',
+        'HOST': 'Localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
